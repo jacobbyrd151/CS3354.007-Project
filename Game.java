7a401b539c1faccfaeefaf0555c047938bf14f1a
Jacob Byrd <jacobbyrd151@gmail.com>
2019-04-09 17:11:10 -0500

    Updated code for Card, Game, Player
    Added skeletons of the classes and implemented some of the methods

---
+++ Game.java
@@ -0,0 +1,7 @@
+//This class will serve as the main class for the program. It will house the main function,
+//and all of the central operations for the card game Black Jack.
+
+public class Game
+{
+	
+}
