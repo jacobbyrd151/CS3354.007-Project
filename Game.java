afd18fd5dcafbb7a153e32c2217345e48d717df8
FlameHarbinger <47788548+FlameHarbinger@users.noreply.github.com>
2019-04-09 17:11:29 -0500

    Update Game.java
    Part of the game class didnt push so I am adding it now

--- Game.java
+++ Game.java
@@ -1,13 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author pyro5656
- */
 public class Game {
+    String name;
+    String type;
+    Player[] turnOrder = new Player[0];
+    Player dealer = new Player(type);
+    
+    public static void Main(String[] args)
+    {
+        
+    }
+    public void phases(Player player, String type)
+    {
+        
+    }
+    public Player checkWin(String type, Player turnOrder)
+    {
+        return turnOrder;
+    }
     
 }
